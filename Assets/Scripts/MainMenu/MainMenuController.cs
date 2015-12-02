f66ad87b126fc1f8d8e95a5c4aed30bc7f4bb470
John Alson Entuna - Desktop <johnalson.entuna@benilde.edu.ph>
2015-12-02 22:49:51 +0800

    [+] Added base project folder and base scripts and scenes

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// MainMenuController is a script that will be referenced by the UI to execute certain commands when you interact with the GUI in the MainMenu
+/// </summary>
+public class MainMenuController : MonoBehaviour
+{
+    /// <summary>
+    /// Used for Unity UI wherein you will be calling this function in the inspector of the 
+    /// UI that you will be executing then in the parameter you will be putting the name of 
+    /// the scene that you will be loading
+    /// </summary>
+    /// <param name="sceneToLoad"></param>
+    public void LoadScene(string sceneToLoad)
+    {
+        Application.LoadLevel(sceneToLoad);
+    }
+
+    /// <summary>
+    /// Used to Quit or Exit the game when executed in the UI
+    /// </summary>
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
